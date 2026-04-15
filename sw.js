bd4d714e3e4d766a05d1e16e5452833deefda39c
ethicalfive <antlers.of.justice@gmail.com>
2026-04-15 16:10:15 +0800

    Update service worker cache name and pre-cache files

--- sw.js
+++ sw.js
@@ -1,11 +1,15 @@
-const CACHE_NAME = 'media-app-v1';
+const CACHE_NAME = 'media-manager-v1';
+const PRE_CACHE = ['./index.html', './manifest.json'];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(['index.html', 'manifest.json']);
-    })
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(PRE_CACHE))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', (event) => {
